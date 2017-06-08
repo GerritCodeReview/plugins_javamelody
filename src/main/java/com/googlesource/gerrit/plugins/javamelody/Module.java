00e5a1c76ce65189f588f6dbcb64f19209b91089
Darragh Bailey <dbailey@hpe.com>
2017-06-08 16:14:56 +0100

    Default to using plugin data dir for new installations
    Change Javamelody to default to using the <site_path>/data/javamelody
    path for it's graph data. This makes it persist by default for
    environments where /tmp is cleared after each reboot, or for containers
    where it is lost on each container restart.
    
    Javamelody defaults to using /tmp/javamelody/<app>_<hostname>. This can
    be surprising as all other data from Gerrit is located under the sites
    path.
    
    Switch the code to use the same mechanism as the javamelody library to
    determine the expected path for the system tmp directory, and should it
    locate an existing /tmp/javamelody/ path, use the old path by default.
    
    For all installations, if the user has not set the system property, then
    if the old path does not exist, set the default path to
    <site_path>/data/javamelody or use what the user has configured via
    gerrit.config container.javaOptions value of
    -Djavamelody.storage_directory parameter.
    
    This should allow for metric data to be persisted by default through
    system/container restarts.
    
    Change-Id: I1ae7013d86cf429f0d15320aafdde18002409e78

--- src/main/java/com/googlesource/gerrit/plugins/javamelody/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/javamelody/Module.java
@@ -21,16 +21,25 @@ import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.webui.TopMenu;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Module extends AbstractModule {
 
   private final PluginConfig cfg;
+  private final String pluginName;
+  private final Path TEMPORARY_DIRECTORY = Paths.get(System.getProperty("java.io.tmpdir"));
+  private final Path tmpDir;
 
   @Inject
-  public Module(PluginConfigFactory cfgFactory, @PluginName String pluginName) {
+  public Module(PluginConfigFactory cfgFactory, @PluginName String pluginName, SitePaths site) {
     this.cfg = cfgFactory.getFromGerritConfig(pluginName);
+    this.tmpDir = site.tmp_dir;
+    this.pluginName = pluginName;
   }
 
   @Override
@@ -40,6 +49,23 @@ public class Module extends AbstractModule {
         .to(MonitoringCapability.class);
     if (cfg.getBoolean("allowTopMenu", true)) {
       DynamicSet.bind(binder(), TopMenu.class).to(MonitoringTopMenu.class);
+    }
+
+    // default to old path for javamelody storage-directory if it exists
+    // otherwise prefer the site tmp location as it is likely persistant.
+    final Path directory = TEMPORARY_DIRECTORY.resolve(pluginName);
+    final Path storage_directory;
+    if (Files.exists(directory) && Files.isDirectory(directory)) {
+      storage_directory = directory;
+    } else {
+      storage_directory = tmpDir.resolve(pluginName);
+    }
+
+    // avoid overriding if explicitly set by user via system properties
+    if (System.getProperty(pluginName + ".storage-directory") == null) {
+      System.setProperty(
+          pluginName + ".storage-directory",
+          cfg.getString("storage_directory", storage_directory.toString()));
     }
   }
 }
