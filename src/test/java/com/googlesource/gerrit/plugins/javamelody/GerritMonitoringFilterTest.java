1f66d66b231b303974b007f3cd95c01e96898f1d
Jacek Centkowski <jcentkowski@collab.net>
2017-11-16 13:02:13 +0100

    Group Javamelody HTTP request statistics
    Currently Javamelody treats every request as specific therefore it
    generates statistics only when one requests the same operation again
    e.g. opens the same review. On busy servers it may additionally lead to
    the issue described in [1].
    
    If no grouping is configured with -Djavamelody.http-transform-pattern
    parameter to container.javaOptions of gerrit.config then the default
    pattern is applied and ids like SHA-1, change-id, project, filename,
    etc. are replaced with '$' and grouped by Javamelody.
    
    [1] https://stackoverflow.com/questions/19147762/javamelody-crashing-the-server-with-thousands-of-rrd-files
    Change-Id: Ie7edf0e7a1482713dc6fd01db8c53bc589c6290f
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

---
+++ src/test/java/com/googlesource/gerrit/plugins/javamelody/GerritMonitoringFilterTest.java
@@ -0,0 +1,94 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.javamelody;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+public class GerritMonitoringFilterTest {
+  private static final Pattern GROUPING_PATTERN =
+      Pattern.compile(GerritMonitoringFilter.JavamelodyFilter.GERRIT_GROUPING);
+
+  @Test
+  public void testGroupingPattern() throws Exception {
+    String dollar = "\\$";
+    String result =
+        GROUPING_PATTERN
+            .matcher(
+                "/changes/?q=test_repo~refs/meta/config~Iee4ae093988f8e2a1ad6d413450eec48b67124a2")
+            .replaceAll(dollar);
+    assertThat(result).named("Change ID triplet grouping").isEqualTo("/changes/?q=$");
+
+    result =
+        GROUPING_PATTERN
+            .matcher(
+                "/plugins/lfs/content/default/a55dc67374da05f4e1eb736f8ad2147d0a6964ed41d28462fd7e2fe86bea78ed")
+            .replaceAll(dollar);
+    assertThat(result).named("Long SHA (LFS) grouping").isEqualTo("/plugins/lfs/content/default/$");
+
+    result =
+        GROUPING_PATTERN
+            .matcher("/changes/?q=05810c7a315ba6c52c150c237d84d05b2ebc3086&n=")
+            .replaceAll(dollar);
+    assertThat(result).named("SHA-1 grouping").isEqualTo("/changes/?q=$&n=");
+
+    result =
+        GROUPING_PATTERN
+            .matcher("/gerrit_ui/gwt/chrome/7CF1DE6EF2AABFEFAE4D469A16D60071.cache.css")
+            .replaceAll(dollar);
+    assertThat(result).named("GWT cache grouping").isEqualTo("/gerrit_ui/gwt/chrome/$.cache.css");
+
+    result = GROUPING_PATTERN.matcher("/files/%2FCOMMIT_MSG/reviewed").replaceAll(dollar);
+    assertThat(result).named("Grouping by file").isEqualTo("/files/$reviewed");
+
+    result =
+        GROUPING_PATTERN.matcher("/projects/test_repo/lfs~lfs:config-project").replaceAll(dollar);
+    assertThat(result).named("Grouping by projects").isEqualTo("/projects/$lfs~lfs:config-project");
+
+    result = GROUPING_PATTERN.matcher("/accounts/self/avatar.change.url").replaceAll(dollar);
+    assertThat(result).named("Grouping by account").isEqualTo("/accounts/$avatar.change.url");
+
+    result = GROUPING_PATTERN.matcher("/test_repo/git-upload-pack").replaceAll(dollar);
+    assertThat(result).named("Grouping git-upload-pack").isEqualTo("$/git-upload-pack");
+
+    result = GROUPING_PATTERN.matcher("/test_repo/git-receive-pack").replaceAll(dollar);
+    assertThat(result).named("Grouping git-receive-pack").isEqualTo("$/git-receive-pack");
+
+    result = GROUPING_PATTERN.matcher("/test_repo.git/info/lfs/locks").replaceAll(dollar);
+    assertThat(result).named("Grouping Git and LFS operations").isEqualTo("$/info/lfs/locks");
+
+    result = GROUPING_PATTERN.matcher("/changes/30/revisions/1/commit").replaceAll(dollar);
+    assertThat(result).named("Grouping numbers").isEqualTo("/changes/$/revisions/$/commit");
+
+    // grouping multiple patters in one input
+    result =
+        GROUPING_PATTERN
+            .matcher(
+                "/changes/30/revisions/05810c7a315ba6c52c150c237d84d05b2ebc3086/files/%2FCOMMIT_MSG/diff")
+            .replaceAll(dollar);
+    assertThat(result)
+        .named("Grouping by number, SHA-1 and file")
+        .isEqualTo("/changes/$/revisions/$/files/$diff");
+
+    result =
+        GROUPING_PATTERN
+            .matcher(
+                "/test_repo.git/info/lfs/locks/8020e4344e49a6928e03b6db69ead86624ffb4aeb1477a7d741a5e2fee9544cd/unlock")
+            .replaceAll(dollar);
+    assertThat(result).named("Grouping LFS with Long SHA").isEqualTo("$/info/lfs/locks/$/unlock");
+  }
+}
